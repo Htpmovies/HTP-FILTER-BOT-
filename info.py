870e5ea7ef49eb3f3efd64bf8a8b5b73e5d0fee0
Htpmovies <123440825+Htpmovies@users.noreply.github.com>
2023-01-24 13:56:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Htp_movies: htp_6667@cluster0.5p0kc2z.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "LazyDeveloper")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
