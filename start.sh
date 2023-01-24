f0591fd4dcccdae4b662e4a0a53c291cfa86a226
Htpmovies <123440825+Htpmovies@users.noreply.github.com>
2023-01-24 13:47:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/Htpmovies/HTP-FILTER-BOT-.git /Htpmovies
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /Htpmovies
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
